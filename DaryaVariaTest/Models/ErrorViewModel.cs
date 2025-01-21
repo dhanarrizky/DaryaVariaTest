91bf1982d8b7bbaa1daa793eb311d2005483161a
dhanarrizky <dhanarrizky23@gmail.com>
2025-01-21 16:17:20 +0700

    created projects

---
+++ DaryaVariaTest/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DaryaVariaTest.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
